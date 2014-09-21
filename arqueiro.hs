ddd29fa3358435778c0d480bf3a49a9805502b6b
carol15022 <carol15022@hotmail.com>
2014-09-21 04:36:51 -0300

    Functions to create the history of the game

--- arqueiro.hs
+++ arqueiro.hs
@@ -1,8 +1,33 @@
-data ArvBin a = Null
-	| No String (ArvBin a) (ArvBin a)
+data Tree a = Null
+	| Node String (Tree a) (Tree a) 
 
-searchString :: ArvBin a -> String -> Bool
-searchString Null value = False
-searchString (No x left right) value 
-	| value == x = True
- 	| otherwise = False || (searchString left value) || (searchString right value)
+historyTree :: Tree [Char]
+historyTree = 
+  Node "Arqueiro"
+    (Node "Montanha" Null Null)
+    (Node "Floresta" Null Null)
+
+searchString :: Tree [Char] -> [Char] -> [Char]
+searchString Null value = ""
+searchString (Node x left right) value 
+	| value == x = x
+ 	| value /= x = (searchString left value)
+
+breakTree :: Tree [Char] -> String -> Tree [Char]
+breakTree Null value = Null
+breakTree (Node a left right) value
+	| value == "right" = right
+	| value == "left" = left
+
+printNode :: Tree [Char] -> [Char]
+printNode Null = ""
+printNode (Node a left right) = a
+
+	
+main = do
+  let holdLeft = (breakTree historyTree "left")
+  let holdRight = (breakTree historyTree "right")
+  print ("Escolha a caminhada entre " ++ (printNode holdLeft) ++ " ou " ++ (printNode holdRight))
+
+  
+  
