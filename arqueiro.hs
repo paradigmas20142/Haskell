38693497168ec77a93114b7b223e508ffdbc6d48
carol15022 <carol15022@hotmail.com>
2014-09-21 12:44:31 -0300

    Second level of the tree

--- arqueiro.hs
+++ arqueiro.hs
@@ -4,8 +4,8 @@ data Tree a = Null
 historyTree :: Tree [Char]
 historyTree = 
   Node "Arqueiro"
-    (Node "Montanha" Null Null)
-    (Node "Floresta" Null Null)
+    (Node "Montanha" (Node "dentro" Null Null) (Node "fora" Null Null))
+    (Node "Floresta" (Node "dentro" Null Null) (Node "fora" Null Null))
 
 searchString :: Tree [Char] -> [Char] -> [Char]
 searchString Null value = ""
@@ -18,16 +18,25 @@ breakTree Null value = Null
 breakTree (Node a left right) value
 	| value == "right" = right
 	| value == "left" = left
+	| value == "all" = (Node a left right)
 
 printNode :: Tree [Char] -> [Char]
 printNode Null = ""
 printNode (Node a left right) = a
 
-	
+main :: IO ()	
 main = do
   let holdLeft = (breakTree historyTree "left")
   let holdRight = (breakTree historyTree "right")
   print ("Escolha a caminhada entre " ++ (printNode holdLeft) ++ " ou " ++ (printNode holdRight))
-
+  choice <- getLine
+  let fisrtTreeSecondLevel =  (breakTree holdLeft "left")
+  let secondTreeSecondLevel =  (breakTree holdLeft "right")
+  let thirdTreeSecondLevel =  (breakTree holdRight "left")
+  let fourthTreeSecondLevel =  (breakTree holdRight "right")
+  if choice == "Montanha"
+  then print ("Escolha seguir por " ++ (printNode fisrtTreeSecondLevel) ++ " ou " ++ (printNode secondTreeSecondLevel) ++ " da cidade dos dragoes")
+  else print ("Escolha seguir por " ++ (printNode thirdTreeSecondLevel) ++ " ou " ++ (printNode fourthTreeSecondLevel) ++ " da cidade dos ossos")
   
+  return ()
   
