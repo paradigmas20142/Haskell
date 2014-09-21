53557df705787316655cdf73ed30f50fb855dd22
carol15022 <carol15022@hotmail.com>
2014-09-21 20:49:31 -0300

    Modularizing

--- MainArcher.hs
+++ MainArcher.hs
@@ -1,30 +1,6 @@
-data Tree a = Null
-	| Node String (Tree a) (Tree a) 
+module MainArcher where
 
-historyTree :: Tree [Char]
-historyTree = 
-  Node "Arqueiro"
-    (Node "floresta" 
-	(Node "dentro" 
-		(Node "cipos" Null Null) 
-		(Node "escadaria" Null Null)) 
-	(Node "fora" Null Null))
-    (Node "montanha" 
-	(Node "dentro" Null Null) 
-	(Node "fora" 
-		(Node "escalando" Null Null) 
-		(Node "estilingue" Null Null)))
-  
-breakTree :: Tree [Char] -> String -> Tree [Char]
-breakTree Null value = Null
-breakTree (Node a left right) value
-	| value == "right" = right
-	| value == "left" = left
-	| value == "all" = (Node a left right)
-
-printNode :: Tree [Char] -> [Char]
-printNode Null = ""
-printNode (Node a left right) = a
+import FunctionsTree(historyTree, breakTree, printNode)
 
 main :: IO ()	
 main = do
